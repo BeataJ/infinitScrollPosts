87d09d101de0e7af4bf1b1ce1aa10b1a71a12ea6
beata <beata@emc2wave.com>
2020-05-27 15:40:45 -0700

    create getPost()

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const filter = document.getElementById('filter');
 let limit = 3;
 let page = 1;
 
-// https://jsonplaceholder.typicode.com/posts?_limit=3&_page=2
+const getPost = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+};
