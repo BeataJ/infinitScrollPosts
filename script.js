2cc53fa3de2f47de34866afaf8d1a35992d3e03d
beata <beata@emc2wave.com>
2020-05-27 16:21:56 -0700

    create showPost()

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const poastContainer = document.getElementById('post-container');
+const postContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
 let limit = 3;
 let page = 1;
 
+// Fetch posts from ADI
 const getPost = async () => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
@@ -14,3 +15,24 @@ const getPost = async () => {
 
   return data;
 };
+
+// Show posts in DOM
+const showPost = async () => {
+  const posts = await getPost();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+    postContainer.appendChild(postEl);
+  });
+};
+
+// show initial posts
+showPost();
